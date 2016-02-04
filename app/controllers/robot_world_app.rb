ebd09a4266cfc23a5510029b217a57a8bee858d8
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-02-03 23:38:04 -0700

    CRUD functionality completed

---
+++ app/controllers/robot_world_app.rb
@@ -0,0 +1,54 @@
+require 'yaml/store'
+
+class RobotWorldApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/robots' do
+    @robots = robot_world.all
+    erb :index
+  end
+
+  get '/robots/new' do
+    erb :new_robot
+  end
+
+  post '/robots' do
+    robot_world.create(params[:robot])
+    redirect'/robots'
+  end
+
+  get '/robots/:id' do |id|
+    @robot = robot_world.find(id.to_i)
+    erb :robo_show
+  end
+
+  get '/robots/:id/edit' do |id|
+    @robot = robot_world.find(id.to_i)
+    erb :robo_edit
+  end
+
+  put '/robots/:id' do |id|
+    robot_world.update(params[:robot], id.to_i)
+    redirect '/robots'
+  end
+
+  delete '/robots/:id' do |id|
+    robot_world.delete(id.to_i)
+    redirect '/robots'
+  end
+
+  not_found do
+    erb :error
+  end
+
+  def robot_world
+    database = YAML::Store.new('db/robot_world')
+    @robot_world ||= RobotWorld.new(database)
+  end
+
+end
---
+++ app/models/robot_world.rb
@@ -0,0 +1,68 @@
+require 'yaml/store'
+
+class RobotWorld
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(robot)
+    database.transaction do
+      database["robots"] ||= []
+      database["total"] ||= 0
+      database["total"] += 1
+      database["robots"] << { "id" => database["total"], "name" => robot[:name],
+                              "city" => robot["city"], "state" => robot["state"],
+                              "avatar" => nil, "birthdate" => robot["birthdate"],
+                              "date_hired" => robot["date_hired"],
+                              "department" => robot["department"]}
+
+    end
+  end
+
+  def raw_tasks
+    database.transaction do
+      database['robots'] || []
+    end
+  end
+
+  def all
+    raw_tasks.map { |data| Robot.new(data)}
+  end
+
+  def raw_task(id)
+    raw_tasks.find {|robot| robot["id"] == id}
+  end
+
+  def find(id)
+    Robot.new(raw_task(id))
+  end
+
+  def update(robot, id)
+    database.transaction do
+      target = database['robots'].find {|robot| robot["id"] == id}
+      target["name"] = robot[:name]
+      target["city"] = robot[:city]
+      target["state"] = robot[:state]
+      target["birthdate"] = robot[:birthdate]
+      target["date_hired"] = robot[:date_hired]
+      target["department"] = robot[:department]
+    end
+  end
+
+  def delete(id)
+    database.transaction do
+      database['robots'].delete_if { |robot| robot["id"] == id }
+    end
+  end
+
+  def delete_all #hacky method written for test helper
+    database.transaction do
+      database['robots'] = []
+      database['total'] = 0
+    end
+  end
+
+
+end
